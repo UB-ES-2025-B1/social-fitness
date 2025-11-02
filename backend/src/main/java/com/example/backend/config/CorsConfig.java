c31393262d18e78594e858cc2677742d19f1534c
Oscar Segura <ooskarsegura@gmail.com>
2025-11-02 20:59:51 +0100

    Print añadido para encontrar error de CorsConfig

--- backend/src/main/java/com/example/backend/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
 
   @Bean
   public WebMvcConfigurer corsConfigurer() {
+    System.out.println(">>> Allowed origins: " + allowedOrigins);
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
