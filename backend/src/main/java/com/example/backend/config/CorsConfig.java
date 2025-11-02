9ca61ffc4b5368114ab62edce260ead179550305
Oscar Segura <ooskarsegura@gmail.com>
2025-11-02 20:46:51 +0100

    CorConfig environment variable name changed for frontend deployment

--- backend/src/main/java/com/example/backend/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-  @Value("${app.cors.allowed-origins:*}")
+  @Value("${ALLOWED_ORIGINS:*}")
   private String allowedOrigins;
 
   @Bean
