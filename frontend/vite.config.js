133ee88ca7e661a20ed08bf4ebd8a9f623d3c605
guillemnavarrapanos <guillemnavarra@hotmail.com>
2025-11-18 14:36:40 +0100

    Primera implementació de QA per a que es passin alguns testos automàticament quan es fa una PR(fix)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,5 +12,6 @@ export default defineConfig({
     setupFiles: './src/setupTests.js',
     globals: true,
     css: true, // permite importar CSS en tests
+    pool: 'forks', // Fix for webidl-conversions errors in CI
   },
 })
